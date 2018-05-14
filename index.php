d79d3627881957aa852c796d6182bf842f74d5a9
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2018-05-14 10:27:57 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -7,6 +7,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <!-- Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-119029375-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-119029375-1');
+    </script>
+
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Home - Filminhos</title>
@@ -24,12 +34,11 @@
             <?php
                 include("php/conexao.php");
 
-                $comando   = "SELECT * FROM generos ORDER BY descGenero ASC";
-                $resultado = mysqli_query($conexao, $comando);
-                //$linhas    = mysqli_num_rows($resultado);
+                $comandoGen   = "SELECT * FROM generos ORDER BY descGenero ASC";
+                $resultadoGen = mysqli_query($conexao, $comandoGen);
                 $arrayGen  = array();
 
-                while($u = mysqli_fetch_assoc($resultado)){
+                while($u = mysqli_fetch_assoc($resultadoGen)){
                     array_push($arrayGen, $u);
                 }
 
@@ -42,6 +51,19 @@
             </div>
         </li>
         <li class="contHeader"><a href="php/novidades.php">NOVIDADES</a></li>
+        <?php
+            if($_SESSION['choris'] == 1){
+                ?>
+                <li class="contHeader"><a href="choris/index.php">CHORIS</a></li>
+                <?php
+            }
+	    if($_SESSION['projetos'] == 1){
+                ?>
+                <li class="contHeader"><a href="projetos/index.php">PROJETOS</a></li>
+                <?php
+            }
+        ?>
+
         <?php
             if($_SESSION['usuarioLogado'] == 1){
                 //botão com nome do usuário redirecionando para a página do mesmo
@@ -64,9 +86,41 @@
             </form>
         </li>
     </ul>
+    <br/>
+    <br/>
     <!-- FIM DO HEADER -->
-
-    
+    <?php
+        $comandoFilmes = "SELECT * FROM filmes ORDER BY nomeFilme ASC";
+        $resultadoFilmes = $conexao->query($comandoFilmes) OR trigger_error($conexao->error, E_USER_ERROR);
+    ?>
+        <?php
+        while($filmes = $resultadoFilmes->fetch_object()){
+        ?>
+            <table align="center" bgcolor="#969fa3" class="tblFilme">
+                <tr>
+                    <td colspan="2">
+                        <h2 class="tituloFilme"><?php echo $filmes->nomeFilme; ?></h2></li>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <center>
+                            <img src=<?php echo $filmes->posterFilme; ?> alt="Perfil" height="250" width="169">
+                        </center>
+                    </td>
+                </td>
+                <tr>
+                    <td>
+                        <p class="diretorFilme">
+                            Diretor: <?php echo $filmes->diretorFilme; ?> | Lançamento: <?php echo $filmes->lancamentoFilme; ?>
+                        </p>
+                    </td>
+                </tr>
+            </table>
+            <br/>
+        <?php
+        }
+        ?>
 </body>
 </html>
 
